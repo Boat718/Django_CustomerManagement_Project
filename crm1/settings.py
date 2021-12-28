60836bc6ee10b6dd198309ec35152d448c534ef9
Boat <Boat_loading@hotmail.com>
2021-12-29 02:17:12 +0700

    Finish

--- crm1/settings.py
+++ crm1/settings.py
@@ -31,20 +31,17 @@ ALLOWED_HOSTS = ['boat-crm-project.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'storages',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    
     'accounts.apps.AccountsConfig',
-    
-    'storages',
-    
-    'django_filters',
 ]
+    
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
